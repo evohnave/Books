3bb1b2ea32af743e5733fb6174534e64e6a962c6
evohnave <eric.vanhove@gmail.com>
2016-03-25 17:57:50 -0400

    Created new section that sets up for Shiny, I think.  Every key:value has its own input field.

--- BookUpdate.R
+++ BookUpdate.R
@@ -1,5 +1,5 @@
 # json style
-
+library(stringi)
 book <- '{"author" : [{"lastName" : "Artuk", "firstName" : "İbrahim"},
                       {"lastName" : "Artuk", "firstName" : "Cevriye"}],
           "title" : "Artukoğulları Sikkeleri",
@@ -17,10 +17,60 @@ book2 <- ',
          }'
 
 book <- paste(book, newPurchaseId(mongo, coll), book2, sep = "")
+#book <- stri_escape_unicode(book)
+validate(book)
+prettify(book)
+insertOne(mongo, coll, book)
+
+
+################################################################################
+###                                                                          ###
+###     New Version!                                                         ###
+###                                                                          ###
+################################################################################
+
+# Note: ğ must be encoded manually as \\u011f, İ as \\u0130, ı as \\u0131
+
+# Data input section
+
+lastName <- "ğĞüÜşŞİıöÖçÇ"
+firstName <- "first names"
+title <- "Title"
+ISBN13 <- "978-3-926195-06-7"
+cover <- "hard cover"
+publisher <- "Sumer Kitabevi"
+coverPictureURL <- "http://www.boyutstore.com/ProductImages/90868/big/byg.shfy.00718.jpg"
+copyright <- 1993
+datePublished <- "January 1993"
+place <- "New York"
+lccn <- "93-12345"
+location  <- "Eric"
+price <- 50.00
+
+# Code section
+Author1 <- '"author" : {"lastName" : "'
+Author2 <- '", "firstName" : "'
+Author3 <- '"}, '
+author <- paste(Author1, stri_escape_unicode(lastName), 
+                Author2, stri_escape_unicode(firstName), Author3, sep = "")
+title <- paste('"title" : "', stri_escape_unicode(title), '", ', paste = "")
+ISBN13 <- paste('"ISBN13" : "', ISBN13, '", ', sep = "")
+purchaseId <- paste('"purchaseId" : ', newPurchaseId(mongo, coll), ', ', sep = "")
+cover <- paste('"cover" : "', cover, '", ', sep = "")
+publisher <- paste('"publisher" : "', publisher, '", ', sep = "")
+coverPicture <- paste('"coverPicture" : "', coverPictureURL, '", ', sep = "")
+copyright <- paste('"copyright" : ', copyright, ', ', sep = "")
+datePublished <- paste('"datePublished" : "', datePublished, '", ', sep = "")
+place <- paste('"place" : "', stri_escape_unicode(place), '", ', sep = "")
+lccn <- paste('"lccn" : "', lccn, '", ', sep = "")
+location <- paste('"location" : "', location, '", ', sep = "")
+price <- paste('"PurchasePrice" : ', price, ', ', sep = "")
+
+book <- paste('{', author, title, ISBN13, purchaseId, cover, coverPicture,
+              copyright, datePublished, place, lccn, location, price, '}',
+              sep = "")
 validate(book)
 prettify(book)
 insertOne(mongo, coll, book)
 
 #          "translator" : {"lastName" : "Tzamali", "firstName" : "Marion"},
-#          "lccn" : "92-14823",
-#          "coverPicture" : "http://ecx.images-amazon.com/images/I/51ZWUV2DEyL.jpg",
